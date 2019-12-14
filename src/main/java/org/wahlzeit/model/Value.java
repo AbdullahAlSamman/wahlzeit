c4d75f9a0d4844cf125499f7d968f665148fe3fe
Abdullah Al Samman <abdullah.al.samman@fau.de>
2019-12-14 20:32:37 +0100

    Value Object class added to VCS

---
+++ src/main/java/org/wahlzeit/model/Value.java
@@ -0,0 +1,28 @@
+package org.wahlzeit.model;
+
+public class Value extends Object {
+
+    private double Value;
+
+    public Value(double value) {
+        Value = value;
+    }
+
+    public double getValue() {
+        return Value;
+    }
+
+    public void setValue(double value) {
+        Value = value;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
